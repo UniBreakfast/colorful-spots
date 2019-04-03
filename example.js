153b15310a3946e08a49b4812367711981936852
Victt0r <dsnobik@gmail.com>
2019-04-03 10:46:31 +0300

    spots

---
+++ example.js
@@ -0,0 +1,25 @@
+context = canvas.getContext('2d')
+canvas.addEventListener('click', clickHandler)
+spots = []
+function clickHandler(event) {
+  var x = event.offsetX,
+      y = event.offsetY,
+      r = Math.random()*40,
+      hue = Math.random()*360,
+      light = 50,
+      spot = {x, y, r, hue, light}
+  spots.push(spot)
+  draw(spot)
+}
+function draw(spot){
+  spot.light+=0.5
+  context.beginPath()
+  context.fillStyle = 'hsl('+spot.hue+', 99%, '+spot.light+'%)'
+  context.arc(spot.x, spot.y, spot.r++, 0, 2 * Math.PI)
+  context.fill()
+}
+function drawSpots(){
+  spots.forEach(draw)
+  spots = spots.filter(spot=>spot.light<101)
+}
+setInterval(drawSpots, 2000)
